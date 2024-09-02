88c07f884a8f8be68a7acc57c3880522bf1b48d0
NithinKumar97 <164483098+NithinKumar97@users.noreply.github.com>
2024-09-02 12:22:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-/*# Fetch the AMI ID from the .bin file in S3
+# Fetch the AMI ID from the .bin file in S3
 data "aws_s3_object" "ami_bin_file" {
   bucket = "testec2ami"  # Replace with your S3 bucket name
   key    = "ami-0dd88b73d06f3d468.bin"  # Replace with your S3 object key
-}*/
+}
 
-# Fetch the AMI ID stored in S3 as a .bin file
+/*# Fetch the AMI ID stored in S3 as a .bin file
 data "aws_s3_object" "ami_bin_file" {
   bucket = "testec2ami"  # Replace with your S3 bucket name
   key    = "ami-0dd88b73d06f3d468.bin"  # Replace with your S3 object key
@@ -18,7 +18,7 @@ data "aws_s3_object" "ami_bin_file" {
 data "local_file" "ami_id" {
   content  = data.aws_s3_object.ami_bin_file.body
   filename = "${path.module}/ami_id.txt"
-}
+}*/
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
@@ -70,8 +70,8 @@ resource "aws_security_group" "allow_ssh" {
 
 # EC2 Instance using AMI stored in S3
 resource "aws_instance" "web" {
-  #ami                         = "ami-0dd88b73d06f3d468"  # Replace with your AMI ID manually
-  ami                         = trimspace(file("${path.module}/ami_id.txt"))
+  ami                         = "ami-0dd88b73d06f3d468"  # Replace with your AMI ID manually
+  #ami                         = trimspace(file("${path.module}/ami_id.txt"))
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_subnet.id
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
